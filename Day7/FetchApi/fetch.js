730cf1b1749f69ffd43d13f906d008c07051fb90
uddhav77 <uddhavlk@gamil.com>
2023-06-08 21:03:02 +0545

    Userfullname

--- Day7/FetchApi/fetch.js
+++ Day7/FetchApi/fetch.js
@@ -6,15 +6,59 @@ const emailEl = document.querySelector('.email')
 const passwordEl = document.querySelector('.password')
 const cpasswordEl = document.querySelector('.cpassword')
 
-formEl.addEventListener('submit',  event =>{
-    event.preventDefault()
-    
-    if (passwordEl.value !== cpasswordEl.value) {
-            alert("Password and Confirm Password do not match")
-        
-            return;
-        }
 
+// formEl.addEventListener('submit',  event =>{
+//     event.preventDefault()
+    
+//     if (passwordEl.value !== cpasswordEl.value) {
+//             alert("Password and Confirm Password do not match")
+        
+//             return;
+//         };
+
+//     async function postData(){
+    
+//         let data = {
+//             firstName: fName.value,
+//             lastName: lName.value,
+//             email: emailEl.value,
+//             password: passwordEl.value,
+//             cpassword: cpasswordEl.value
+//         };
+    
+//     try{
+//     const response = await fetch('https://rest-api-bjno.onrender.com/register',{
+//         method: 'POST',
+//         headers: {
+//             'Content-Type' : 'application/json'
+//         },
+
+//         body: JSON.stringify(data)
+        
+//     })
+    
+//     const value = await response.json();
+//     console.log(value)
+
+//     const {firstName, lastName} = value
+//     const userlistEL = document.querySelector('#userul')
+//     const listEl = document.createElement('.userul')
+//     console.log(firstName, lastName)
+//     listEl.textContent = `Full Name: ${firstName} ${lastName}`
+//     userlistEL.appendChild(listEl)
+
+//     } catch{
+//         (err) => console.log('Error')
+//     }
+
+// }
+// })
+
+
+const dispbtn = document.querySelector('.disp')
+
+dispbtn.addEventListener('click',getData)
+async function getData(){
     let data = {
         firstName: fName.value,
         lastName: lName.value,
@@ -23,20 +67,32 @@ formEl.addEventListener('submit',  event =>{
         cpassword: cpasswordEl.value
     };
 
-    fetch('https://rest-api-bjno.onrender.com/register',{
-        method: 'POST',
-        headers: {
-            'Content-Type' : 'application/json'
-        },
-
-        body: JSON.stringify(data)
+    try{
+    const response = await fetch('https://rest-api-bjno.onrender.com/users', {
+        method:'GET',
         
     })
-    .then((response) => response.json())
-    .then((value)=> console.log(value))
-    .catch((err)=> {console.log('Error')})
 
-})
+    const value =await response.json()
+    console.log(value)
+
+
+    const reglist = document.getElementById('regul')
+    for(let i=0; i< value.length ; i++){
+        const item = value[i]
+        const fullName = item.firstName + ' '+ item.lastName
+        const listEle = document.createElement('li')
+        listEle.textContent = fullName
+        reglist.appendChild(listEle)
+    }
+    } catch{
+        (err) => console.log('Error')
+    }
+}
+
+
+
+
 
 
 
